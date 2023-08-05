e44f82bab0dc9ca98eb2b17d3399dcfe6b4489ad
Guilherme <borimdev@gmail.com>
2023-08-05 11:06:41 -0300

    estruturando banner header

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function styles() {
-    return gulp.src('./src/styles/*.scss')
+    return gulp.src('./src/styles/**/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest('./dist/css'));
 }
 
 exports.default = styles;
 exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/styles/**/*.scss', gulp.parallel(styles))
 } 
